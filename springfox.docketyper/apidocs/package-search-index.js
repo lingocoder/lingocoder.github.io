03398df03bcdb29862791120c0a575963d458412
lingocoder <coder@lingocoder.com>
2020-09-06 00:58:19 +0000

    Make using SpringFox with JPMS possible

---
+++ springfox.docketyper/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.lingocoder.springfox.api.doc"},{"l":"com.lingocoder.springfox.enums"}]
---
+++ springfox.docketyper/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.lingocoder.springfox.enums","l":"DocketType"},{"p":"com.lingocoder.springfox.api.doc","l":"DocketTyper"}]
